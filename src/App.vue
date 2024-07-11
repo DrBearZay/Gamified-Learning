8c1ccfde4b236859dc4870190e08bf3a39b1ceb1
zhouaoyu <zhouaoyu@eastmoney.com>
2024-07-11 16:50:24 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <HomePage></HomePage>
+</template>
+
+<script setup lang="ts">
+import HomePage from './components/HomePage.vue';
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex gap-8 items-start">
+    <InstructionSelector></InstructionSelector>
+    <CodeArea></CodeArea>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import CodeArea from '@/components/CodeArea.vue';
+import InstructionSelector from '@/components/InstructionSelector.vue';
+</script>
+
+<style lang="scss" scoped></style>
