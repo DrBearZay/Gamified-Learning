2a0aa6096a3dfcdf43ce8f3fa804e727919b1d13
DrBearZay <zay586754@163.com>
2024-09-22 00:07:48 +0800

    feat: ✨ 调整代码模块逻辑

--- src/components/game/InstructionList.vue
+++ src/components/game/InstructionList.vue
@@ -5,10 +5,10 @@
       <Container
         behaviour="copy"
         group-name="1"
-        :get-child-payload="getChildPayload1"
+        :get-child-payload="getAddPayload"
         class="draggable-container"
       >
-        <Draggable v-for="item in items1" :key="item.id">
+        <Draggable v-for="item in instructionList" :key="item.id">
           <div class="draggable-item">
             {{ item.data }}
           </div>
@@ -16,14 +16,15 @@
       </Container>
     </div>
     <div>
+      <!-- todo 增加行号 -->
       <Container
         group-name="1"
-        :get-child-payload="getChildPayload2"
-        @drop="onDrop2"
+        :get-child-payload="getSwapPayload"
+        @drop="addInstruction"
         :remove-on-drop-out="true"
         class="draggable-container"
       >
-        <Draggable v-for="item in items2" :key="item.id">
+        <Draggable v-for="item in userCodeList" :key="item.id">
           <div class="draggable-item">
             {{ item.data }}
           </div>
@@ -34,65 +35,73 @@
 </template>
 
 <script setup lang="ts">
+import { nanoid } from 'nanoid';
 import { ref } from 'vue';
 import { Container, Draggable } from 'vue3-smooth-dnd';
 
+type PayloadType = {
+  opr: string;
+  param?: number;
+};
+type InstructionType = {
+  id: string | number;
+  data: string;
+  payload: PayloadType;
+};
 // item1里复制出来的id不能写死
-const items1 = ref([
-  { id: 1, data: 'Input' },
-  { id: 2, data: 'Output' },
-  { id: 3, data: 'Add' },
-  { id: 4, data: 'Sub' },
+// 可用指令
+const instructionList = ref<InstructionType[]>([
+  { id: 'Input', data: 'Input', payload: { opr: 'Input' } },
+  { id: 'Output', data: 'Output', payload: { opr: 'Output' } },
+  // todo 可以通过颜色来区分同一组内的jump和distination
+  { id: 'Jump', data: 'Jump', payload: { opr: 'Jump' } },
+  // { id: 'Destination', data: 'Destination' },
+  { id: 'Add', data: 'Add', payload: { opr: 'Add' } },
+  { id: 'Sub', data: 'Sub', payload: { opr: 'Sub' } },
 ]);
-
-const items2 = ref([
-  { id: 1, data: '1' },
-  { id: 2, data: '2' },
-  { id: 3, data: '3' },
-  { id: 4, data: '4' },
+// 用户代码
+const userCodeList = ref<InstructionType[]>([
+  { id: 'Input', data: 'Input', payload: { opr: 'Input' } },
+  { id: 'Output', data: 'Output', payload: { opr: 'Output' } },
 ]);
 
 type dropResultType = {
   removedIndex: number;
   addedIndex: number;
-  payload: string;
+  payload: PayloadType;
 };
-const onDrop2 = (dropResult: dropResultType) => {
+const addInstruction = (dropResult: dropResultType) => {
   console.log(dropResult);
-  items2.value = applyDrag(items2.value, dropResult);
-};
-
-const applyDrag = (arr: { id: number; data: string }[], dragResult: any) => {
-  const { removedIndex, addedIndex, payload } = dragResult;
-  if (removedIndex === null && addedIndex === null) return arr;
-
-  const result = [...arr];
-  let itemToAdd = { id: arr.length + 1, data: payload };
-
+  const { addedIndex, removedIndex, payload } = dropResult;
+  let instructionToAdd: InstructionType = {
+    data: payload.opr,
+    id: nanoid(),
+    payload,
+  };
+  // todo 根据指令的opr在添加和删除时进行不同的操作
+  // 删除指令
   if (removedIndex !== null) {
-    itemToAdd = result.splice(removedIndex, 1)[0];
+    instructionToAdd = userCodeList.value.splice(removedIndex, 1)[0];
   }
-
+  // 添加指令
   if (addedIndex !== null) {
-    result.splice(addedIndex, 0, itemToAdd);
+    userCodeList.value.splice(addedIndex, 0, instructionToAdd);
   }
-
-  return result;
 };
 
-const getChildPayload1 = (index: number) => {
-  console.log('111');
-  return items1.value[index].data;
+const getAddPayload = (index: number) => {
+  // console.log('add-instruction');
+  return instructionList.value[index].payload;
 };
-const getChildPayload2 = (index: number) => {
-  console.log('222');
-  return items2.value[index].data;
+const getSwapPayload = (index: number) => {
+  // console.log('swap-instruction');
+  return userCodeList.value[index].payload;
 };
 </script>
 
 <style lang="scss" scoped>
 .draggable-container {
-  @apply overflow-y-auto h-[300px] w-[200px] bg-gray-100 rounded-md shadow-md px-3 py-2;
+  @apply overflow-y-auto h-full w-[200px] bg-gray-100 rounded-md shadow-md px-3 py-2;
 }
 .draggable-item {
   @apply bg-yellow-500 my-2 p-1 cursor-move rounded-md shadow-md;
